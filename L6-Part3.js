e9189c907296a1f721606b647096b4faa8d4c659
su7163if <stuff@stuff.com>
2018-02-13 19:32:46 -0600

    started part 3

---
+++ L6-Part3.js
@@ -0,0 +1,14 @@
+/**
+ * Created by su7163if on 2/13/2018.
+ */
+$(function () {
+    $("span").css({
+        "font-weight" : "bold"
+    });
+
+    $("h2").click(function(){
+        $(this).css({
+            "background-color" : "green"
+        })
+    });
+});
